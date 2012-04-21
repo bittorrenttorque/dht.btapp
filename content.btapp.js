02d499e93a7125fff85812ff60a4dc222a683501
Patrick Williams <pwmckenna@gmail.com>
2012-04-21 12:59:25 -0700

    refactored...also taking advantage of some new client events being triggered by the base btapp object

--- content.btapp.js
+++ content.btapp.js
@@ -9,28 +9,27 @@
 	BtappContent = Backbone.Model.extend({
 		initialize: function() {
 			window.btapp = new Btapp;
-			btapp.on('add:dht', function(dht) {
-				if('get_any_hash' in dht) {
-					dht.get_any_hash(function() {}, this.hash);
-				} else {
-					dht.on('add:get_any_hash', function() {
-						btapp.get('dht').get_any_hash(function() {}, this.hash);
-					});
-				}
-			}, this);
+			btapp.on('torrentStatus', this.status, this);
+			btapp.on('add:dht', this.setup_dht, this);
 
-			btapp.on('add:torrent', function(list) {
-				list.on('add', this.torrent, this);
-				list.each(this.torrent, this);
-			}, this);
-
-			btapp.on('add:events', function(events) {
-				events.save({'torrentStatus': function(status) {
-					debugger;
-				}});
-			}, this);
-
+			btapp.on('add:torrent', this.setup_torrents, this);
 			btapp.connect();
+		},
+		setup_dht: function(dht) {
+			if('get_any_hash' in dht) {
+				dht.get_any_hash(function() {}, this.hash);
+			} else {
+				dht.on('add:get_any_hash', function() {
+					btapp.get('dht').get_any_hash(function() {}, this.hash);
+				});
+			}
+		},
+		setup_torrents: function(list) {
+			list.on('add', this.torrent, this);
+			list.each(this.torrent, this);
+		},
+		status: function(status) {
+			debugger;
 		},
 		hash: function(hash) {
 			console.log('hash: ' + hash);
@@ -39,6 +38,9 @@
 		},
 		torrent: function(torrent) {
 			console.log('torrent:' + torrent.id);
+		},
+		file: function(torrent) {
+
 		}
 	});
 }).call(this);
