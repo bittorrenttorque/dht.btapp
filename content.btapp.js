6cf02bb1dddefb361c9840961b7d99ebc8a40bd8
Patrick Williams <patrick@bittorrent.com>
2012-04-22 10:40:04 -0700

    almost done...have a single point when torrents are ready to be processed

--- content.btapp.js
+++ content.btapp.js
@@ -14,6 +14,12 @@
 
 			btapp.on('add:torrent', this.setup_torrents, this);
 			btapp.connect();
+			this.setup_status_handlers();
+		},
+		setup_status_handlers: function() {
+			this.torrent_status_handlers = {};
+			this.torrent_status_handlers[Btapp.STATUS.TORRENT.ADDED] = this.torrent_added_handler;
+			this.torrent_status_handlers[Btapp.STATUS.TORRENT.METADATA_COMPLETE] = this.torrent_metadata_handler;
 		},
 		setup_dht: function(dht) {
 			if('get_any_hash' in dht) {
@@ -29,18 +35,71 @@
 			list.each(this.torrent, this);
 		},
 		status: function(status) {
-			debugger;
+			if(typeof status !== 'object') return;
+			if(!('status' in status)) return;
+			if(this.torrent_status_handlers[status.state]) {
+				this.torrent_status_handlers[status.state].call(this, status);
+			}
 		},
 		hash: function(hash) {
-			console.log('hash: ' + hash);
+			if(btapp.get('torrent').get(hash)) return;
 			var link = get_magnet_link(hash);
 			btapp.get('add').torrent(function() {}, link);
 		},
 		torrent: function(torrent) {
-			console.log('torrent:' + torrent.id);
+			torrent.set_priority(function() {}, Btapp.TORRENT.PRIORITY.METADATA_ONLY);
 		},
-		file: function(torrent) {
+		torrent_added_handler: function(status) {
+			if(btapp.get('torrent').get(status.hash)) {
+				btapp.get('torrent').get(status.hash).force_start(function() {});
+			} else {
+				btapp.get('torrent').on('add:' + status.hash, function(torrent) {
+					btapp.get('torrent').off('add:' + status.hash);
+					torrent.force_start(function() {});
+				});
+			}
+		},
+		torrent_metadata_handler: function(status) {
+			if(btapp.get('torrent').get(status.hash)) {
+				this.aggregate_torrent_information(btapp.get('torrent').get(status.hash));
+			} else {
+				btapp.get('torrent').on('add:' + status.hash, function(torrent) {
+					this.aggregate_torrent_information(torrent);
+				}, this);
+			}
+		},
+		aggregate_torrent_information: function(torrent) {
+			// At this point we have a torrent that is stopped. 
+			// We don't necessarily have the information
+			// to process it yet however.
+			if(!torrent.get('properties')) {
+				torrent.on('add:properties', function(properties) {
+					this.aggregate_torrent_information(torrent);
+					torrent.off('add:properties');
+				}, this);
+				return;
+			}
+			if(!torrent.get('file')) {
+				torrent.on('add:file', function(files) {
+					this.aggregate_torrent_information(torrent);
+					torrent.off('add:file');
+				}, this);
+				return;
+			}
 
+			if(torrent.get('file').length === 0) {
+				var files = torrent.get('file');
+				files.on('add', function() {
+					this.aggregate_torrent_information(torrent);
+					files.off('add');
+				}, this)
+				return;
+			}
+
+			this.process_torrent_information(torrent);
+		},
+		process_torrent_information: function(torrent) {
+			debugger;
 		}
 	});
 }).call(this);
