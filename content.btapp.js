cfc7ed84fe1f3490b59f543dad82117bdaa42fcc
Patrick Williams <pwmckenna@gmail.com>
2012-04-29 16:38:15 -0700

    adding a bunch of triggered events and no longer relying on windows variables existing...proper scoping

--- content.btapp.js
+++ content.btapp.js
@@ -8,33 +8,40 @@
 
 	BtappContent = Backbone.Model.extend({
 		initialize: function() {
-			var btapp = new Btapp;
-			btapp.on('torrentStatus', this.status, this);
-			btapp.on('add:dht', this.setup_dht, this);
+			_.bindAll(this);
 
-			btapp.on('add:torrent', this.setup_torrents, this);
-			btapp.connect();
+			this.btapp = new Btapp;
+			this.btapp.on('torrentStatus', this.status);
+			this.btapp.on('add:dht', this.setup_dht);
+
+			this.btapp.on('add:torrent', this.setup_torrents);
+			this.btapp.connect();
 			this.setup_status_handlers();
 		},
 		setup_status_handlers: function() {
 			this.torrent_status_handlers = {};
-			this.torrent_status_handlers[Btapp.STATUS.TORRENT.ADDED] = this.torrent_added_handler;
 			this.torrent_status_handlers[Btapp.STATUS.TORRENT.METADATA_COMPLETE] = this.torrent_metadata_handler;
 		},
 		setup_dht: function(dht) {
 			if('get_any_hash' in dht) {
+				this.trigger('content:dht');
 				dht.get_any_hash(this.hash);
 			} else {
-				dht.on('add:get_any_hash', function() {
-					btapp.get('dht').get_any_hash(this.hash);
-				});
+				dht.on('add:get_any_hash', _.bind(function() {
+					this.trigger('content:dht');
+					this.btapp.get('dht').get_any_hash(this.hash);
+				}, this));
 			}
 		},
 		setup_torrents: function(list) {
-			list.on('add', this.torrent, this);
+			list.on('add', this.torrent);
+			list.on('remove', function(value, key) {
+				this.trigger('content:remove', value.id);
+			}, this);
 			list.each(this.torrent, this);
 		},
 		status: function(status) {
+			this.trigger('content:status', status);
 			if(typeof status !== 'object') return;
 			if(!('status' in status)) return;
 			if(this.torrent_status_handlers[status.state]) {
@@ -42,30 +49,26 @@
 			}
 		},
 		hash: function(hash) {
-			if(btapp.get('torrent').get(hash)) return;
+			this.trigger('content:hash', hash);
+			if(this.btapp.get('torrent').get(hash)) return;
 			var link = get_magnet_link(hash);
-			btapp.get('add').torrent(link);
+			this.btapp.get('add').torrent(link);
 		},
 		torrent: function(torrent) {
+			this.trigger('content:torrent', torrent.id);
+			torrent.force_start();
 			torrent.set_priority(Btapp.TORRENT.PRIORITY.METADATA_ONLY);
-		},
-		torrent_added_handler: function(status) {
-			if(btapp.get('torrent').get(status.hash)) {
-				btapp.get('torrent').get(status.hash).force_start();
-			} else {
-				btapp.get('torrent').on('add:' + status.hash, function(torrent) {
-					btapp.get('torrent').off('add:' + status.hash);
-					torrent.force_start();
-				});
-			}
+			this.aggregate_torrent_information(torrent);
 		},
 		torrent_metadata_handler: function(status) {
-			if(btapp.get('torrent').get(status.hash)) {
-				this.aggregate_torrent_information(btapp.get('torrent').get(status.hash));
+			// The client has notified us that we have the metadata for a torrent
+			// It may not have bubbled up to our library, but make sure we stop it
+			// as soon as we see it. We have no use for the content.
+			this.trigger('content:metadata', status);
+			if(this.btapp.get('torrent').get(status.hash)) {
+				this.btapp.get('torrent').get(status.hash).stop();
 			} else {
-				btapp.get('torrent').on('add:' + status.hash, function(torrent) {
-					this.aggregate_torrent_information(torrent);
-				}, this);
+				this.btapp.get('torrent').on('add:' + status.hash, function(torrent) { torrent.stop(); });
 			}
 		},
 		aggregate_torrent_information: function(torrent) {
@@ -102,7 +105,7 @@
 			// Now that we have all the torrent file/metadata information,
 			// Notify whoever is listening, and provide the info...once
 			// that's been done there's no need to add the torrent again.
-			this.trigger('content', torrent.toJSON());
+			this.trigger('content:data', torrent.toJSON());
 			torrent.remove();
 		}
 	});
